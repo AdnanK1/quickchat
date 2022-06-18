7667d21e02d77edefa8e6a0ba9ea2d900571226c
AdnanK1 <adnangitonga@gmail.com>
2022-06-18 16:45:11 +0300

    Base/Templates:Made the navbar.html

--- quickchat/settings.py
+++ quickchat/settings.py
@@ -107,6 +107,15 @@ DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
+CHANNEL_LAYERS = {
+    'default':{
+        'BACKEND':'channels_redis.core.RedisChannelLayer',
+        'CONFIG':{
+            'hosts':[('127.0.0.1', 6379)]
+        }
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
