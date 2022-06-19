ca882f6ebb61c08a9b6714b21ed9680c91b41587
AdnanK1 <adnangitonga@gmail.com>
2022-06-19 13:04:49 +0300

    Base/Views: added the functionality to add new users to the applications

--- base/views.py
+++ base/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render,redirect
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
@@ -10,5 +16,17 @@ def loginPage(request):
     return render(request,'auth/login.html',context)
 
 def registerPage(request):
-    context = {}
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST, request.FILES)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            user = form.save()
+            login(request,user,backend = 'django.contrib.auth.backends.ModelBackend')
+            
+            return redirect('home')
+        else:
+            messages.error(request,'An error has occurred when Logging, please use a stronger password')
+    context = {'form':form}
     return render(request,'auth/register.html',context)
