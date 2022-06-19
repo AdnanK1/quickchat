2295bae25a2a141183e87166b7a1bde167e98358
AdnanK1 <adnangitonga@gmail.com>
2022-06-19 21:18:20 +0300

    Base/forms:made the setupform

--- base/forms.py
+++ base/forms.py
@@ -1,8 +1,15 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm
+from . import models
 
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
+
+class SetUpForm(ModelForm):
+    class Meta:
+        model = models.Client
+        fields = '__all__'
+        exclude = ['user','email']
