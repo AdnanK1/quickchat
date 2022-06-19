cb558d05d9aaa04c6b8ec63024dd458d345318c7
AdnanK1 <adnangitonga@gmail.com>
2022-06-19 13:11:59 +0300

    Base/Views:Added functionality for the an existing user can log in

--- base/views.py
+++ base/views.py
@@ -13,7 +13,25 @@ def home(request):
     return render(request,'home.html',context)
 
 def loginPage(request):
-    context = {}
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist ')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or Password does not exist')
+    context = {'page':page}
     return render(request,'auth/login.html',context)
 
 def registerPage(request):
